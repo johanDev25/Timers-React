b08b8a4347d19f51ec03f7ef508f61aa81a26c71
Johan <johan225@hotmail.es>
2019-11-25 15:13:40 -0500

    Agrega boostrap-uuid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card} from 'react-bootstrap';
+import uuid from 'uuid/v4'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      items: [
+        {id:uuid(), task:"React", project:"Conometro"}
+      ]
+    }
+  }
+  render(){
+    return(
+    <Container>
+        <h3>Hola</h3>
+        <h5>Hola</h5>
+    </Container>
+    )
+  }
 }
 
 export default App;
