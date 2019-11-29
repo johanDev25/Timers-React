9ad01207e408a4c01f6b8033bfa586e6806d7658
Johan <johan225@hotmail.es>
2019-11-29 15:17:44 -0500

    Updates

---
+++ precache-manifest.44773bd6ee707bb28d67cdd9f4f7ab91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2114bc129446d802c02ec6ea9ada9edc",
+    "url": "/Timers-React/index.html"
+  },
+  {
+    "revision": "40e60b4afd2281d9d63a",
+    "url": "/Timers-React/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9e0955dc50088a4f81ea",
+    "url": "/Timers-React/static/css/main.877259fa.chunk.css"
+  },
+  {
+    "revision": "40e60b4afd2281d9d63a",
+    "url": "/Timers-React/static/js/2.801fafb5.chunk.js"
+  },
+  {
+    "revision": "9e0955dc50088a4f81ea",
+    "url": "/Timers-React/static/js/main.bf72dc04.chunk.js"
+  },
+  {
+    "revision": "b67c0b645a48b865a648",
+    "url": "/Timers-React/static/js/runtime-main.6a1bfc3e.js"
+  }
+]);
